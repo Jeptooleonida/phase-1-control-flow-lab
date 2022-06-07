cd654eeed4dafa956451cf9bbd7d7e5db70864f9
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-07 23:23:05 +0300

    Done.

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ function switchOnCharmFromTip(tip){
   switch(tip) {
     case 'generous':
       return 'Thank you so much.';
-  
+    break;
     case 'not as generous':
       return 'Thank you.';
-    
+    break;
     default: 
     return 'Bye.'
   }
