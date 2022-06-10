1fd8f9cba1e96c22d8b5f868faafbae522b4298c
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-10 15:19:58 +0300

    commit tree c355eb2be3874a592d94bc93f4bfa8a232eb3180

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function scuberGreetingForFeet(feet){
   if (feet <= 400){
     return 'This one is on me!' ;
   }
+  else if (400 <= feet && feet < 2000){
+    return 'I will gladly take 25 bucks.';
+  }
   else if ( 2000 <=feet && feet < 2500){
     return 'I will gladly take your thirty bucks.';
   }
